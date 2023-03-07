755e9deebf15dfd9cf077c6609968a294fabbd57
Lorenzo Ugoccioni s315734 <s315734@studenti.polito.it>
2023-03-07 09:05:47 +0100

    modified function.js and object.js files

--- Class_0203/object.js
+++ Class_0203/object.js
@@ -27,3 +27,35 @@ delete movie.genre; //
 for(const prop in movie){
     console.log(`${prop} is ${movie[prop]}`)
 }
+
+let movieKeys = Object.keys(movie);  //extract an array of object's keys
+let movieKVpairs = Objeect.entries(movie); //extract an array of Key-value pairs from the object
+let movieValues = Object.values(movie)  //extract an array of object's values
+
+const ALIASmovie = movie;   //not copiyng the object itself, but only the reference
+                            //ALIASmovie & movie will reference at the same object
+console.log(ALIASmovie);
+
+const COPYmovie = Object.assign({}, movie); //copying all the proprerty of movie object
+                                            // in a fully (empty) new object
+console.log(COPYmovie);
+
+Object.assign(movie, {budget: '1 million USD'});    //I'm merging properties, also more than 1 property
+                                                    //Adding a property (budget) to an existing object (movie)
+console.log(movie);
+console.log(ALIASmovie);
+
+const UPDATENEWmovie = Object.assign({}, movie, {cast: 'Leonardo DiCaprio'}); //I'm merging properties
+                                                                            //Adding a property (budget) to a new empty object ({}),
+                                                                            //copied from an existing object (movie)
+console.log(UPDATENEWmovie);
+
+/*-> The assign(...) method can return a new object that can be assigned to a new variable
+     If i need only to upgrade an existing object, the assignement is not needed;
+     Conversely, it's usefull if I need a completely new object from scratch*/
+
+const otherDUPLICATEDmovie = {... movie};   //spreading the movie properties to be assigned in the new object
+
+console.log(otherDUPLICATEDmovie);
+
+console.log('title' in movie);  //checking if the property it's in the object -> return a boolean
